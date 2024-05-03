9b5ba6589dae3e6584905f197b1b3057a397097b
Oriel <orieldelgado01@gmail.com>
2024-05-03 17:07:27 -0500

    Load Data Complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import utils.LoadData;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Exec - LoadData Class");
+        new Thread(() -> {
+            LoadData load = new LoadData();
+            load.loadProviders();
+            System.out.println("Data Loaded");
+        }).start();
+
+        // Go to respective interface (POV)
+        System.out.println("Select a Role");
     }
 }
--- src/utils/LoadData.java
+++ src/utils/LoadData.java
@@ -12,26 +12,28 @@ public class LoadData {
         Provider.providers.add(p1);
         Provider.providers.add(p2);
         Provider.providers.add(p2);
+        loadProducts();
     }
 
-    public void loadProducts(){
+    private void loadProducts(){
         Product p1 = new Product(1, 1, 1000.00, 1200.00, 2, 900.00, "Laptop");
         Product p2 = new Product(2, 3, 800.00, 670.00, 2, 600.00, "LCD TV");
         Product p3 = new Product(3, 1, 150.00, 200.00, 3, 90.00, "Desktop");
         Product.products.add(p1);
         Product.products.add(p2);
         Product.products.add(p3);
+        //showDataLoaded();
     }
 
-    public void showDataLoaded(){
-        System.out.println("Providers Loaded");
-        for (Provider provider : Provider.providers){
-            System.out.println(provider.toString());
-        }
-        System.out.println("Products Loaded");
-        for (Product product : Product.products){
-            System.out.println(product.toString());
-        }
-    }
+//    private void showDataLoaded(){
+//        System.out.println("Providers Loaded");
+//        for (Provider provider : Provider.providers){
+//            System.out.println(provider.toString());
+//        }
+//        System.out.println("Products Loaded");
+//        for (Product product : Product.products){
+//            System.out.println(product.toString());
+//        }
+//    }
 
 }
